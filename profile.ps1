e18d3cd207e72e09541e38a445c658e709ed0f3b
John D. Fisher <jdfenw@gmail.com>
2025-02-01 14:58:05 -0800

    fix(conda): update conda init

--- profile.ps1
+++ profile.ps1
@@ -1,17 +1,16 @@
-
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
 If (Test-Path "$HOME\miniconda3\Scripts\conda.exe") {
     # (& "$HOME\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
     $Env:CONDA_EXE = "$HOME\miniconda3\Scripts\conda.exe"
-    $Env:_CE_M = ""
-    $Env:_CE_CONDA = ""
+    $Env:_CE_M = $null
+    $Env:_CE_CONDA = $null
     $Env:_CONDA_ROOT = "$HOME\miniconda3"
     $Env:_CONDA_EXE = "$HOME\miniconda3\Scripts\conda.exe"
     $CondaModuleArgs = @{ChangePs1 = $True}
     Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
 
-    conda activate vim-python
+    Invoke-Conda activate vim-python
 
     Remove-Variable CondaModuleArgs
 }
