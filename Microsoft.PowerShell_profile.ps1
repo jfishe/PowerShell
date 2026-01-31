e533bcb283199ce53335861326847385e994508a
John D. Fisher <jdfenw@gmail.com>
2026-01-31 11:00:32 -0800

    feat(starship): auto initialize PSVersion 5-7
    Remove Profile.Starship, which is installation path specific.
    Generate init script if not found.
    
    Uncomment new-tab-same-directory.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -34,8 +34,6 @@ If ($host.Name -eq 'ConsoleHost') {
     . "$PSScriptRoot/Completions/Profile.Completions"
 
     if (Get-Command 'starship' -ErrorAction SilentlyContinue) {
-        # & starship init powershell --print-full-init |
-        #   Out-File -Encoding utf8 -Path "Profile.Starship.ps1"
 
         function Invoke-Starship-PreCommand {
             if ($global:profile_initialized -ne $true) {
@@ -48,14 +46,25 @@ If ($host.Name -eq 'ConsoleHost') {
                 Import-Module -Global -DisableNameChecking -Name posh-git, git-aliases
             }
             # # https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory#powershell-with-starship
-            # $loc = $executionContext.SessionState.Path.CurrentLocation;
-            # $prompt = "$([char]27)]9;12$([char]7)"
-            # if ($loc.Provider.Name -eq "FileSystem") {
-            #     $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-            # }
-            # $host.ui.Write($prompt)
+            $loc = $executionContext.SessionState.Path.CurrentLocation;
+            $prompt = "$([char]27)]9;12$([char]7)"
+            if ($loc.Provider.Name -eq "FileSystem") {
+                $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+            }
+            $host.ui.Write($prompt)
         }
         # Invoke-Expression (&starship init powershell)
-        . "$PSScriptRoot/Profile.Starship.ps1"
+        try {
+            . "$PSScriptRoot/Profile.Starship.ps1"
+        } catch [System.Management.Automation.CommandNotFoundException] {
+            if ($PSVersionTable.PSVersion.Major -lt 6) {
+                & starship init powershell --print-full-init |
+                Out-File -Encoding utf8 -FilePath "$PSScriptRoot/Profile.Starship.ps1"
+            } else {
+                & starship init powershell --print-full-init |
+                Out-File -Encoding utf8 -Path "$PSScriptRoot/Profile.Starship.ps1"
+            }
+            . "$PSScriptRoot/Profile.Starship.ps1"
+        }
     }
 }
